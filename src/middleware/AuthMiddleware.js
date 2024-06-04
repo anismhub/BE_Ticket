82f45240e6f0e7682f938f6a0a70b3f5537c6cdb
iwan0210 <iwancuah13@gmail.com>
2024-06-04 22:47:05 +0700

    get Tickets, error searcj

--- src/middleware/AuthMiddleware.js
+++ src/middleware/AuthMiddleware.js
@@ -1,21 +1,21 @@
 const jwt = require('jsonwebtoken')
 const AuthenticationError =require('../exceptions/AuthenticationError')
 const AuthorizationError =require('../exceptions/AuthorizationError')
-const InvariantError =require('../exceptions/InvariantError')
+const ClientError =require('../exceptions/ClientError')
 
 const verifyToken = (req, _, next) => {
     try {
-        const token = req.header('Authorization')
+        const token = req.header('Authorization').split(' ')[1]
         if (!token) {
             throw new AuthenticationError("Unauthenticated")
         }
         const decode = jwt.verify(token, process.env.SECRET)
         if (!decode.userId) {
-            throw new InvariantError("Invalid Token")
+            throw new ClientError("Invalid Token")
         }
         req.userId = decode.userId
         req.userName = decode.userName
-        req.role = decode.role
+        req.userRole = decode.userRole
         next()
     } catch (error) {
         next(error)
@@ -24,20 +24,17 @@ const verifyToken = (req, _, next) => {
 
 const verifyTechToken = (req, _, next) => {
     try {
-        const token = req.header('Authorization')
+        const token = req.header('Authorization').split(' ')[1]
         if (!token) {
             throw new AuthenticationError("Unauthenticated")
         }
         const decode = jwt.verify(token, process.env.SECRET)
         if (!decode.userId) {
-            throw new InvariantError("Invalid Token")
+            throw new ClientError("Invalid Token")
         }
         if (decode.role != 'Teknisi') {
             throw new AuthorizationError("Anda tidak berhak mengakses resource ini")
         }
-        req.userId = decode.userId
-        req.userName = decode.userName
-        req.role = decode.role
         next()
     } catch (error) {
         next(error)
@@ -46,20 +43,17 @@ const verifyTechToken = (req, _, next) => {
 
 const verifyAdminToken = (req, _, next) => {
     try {
-        const token = req.header('Authorization')
+        const token = req.header('Authorization').split(' ')[1]
         if (!token) {
             throw new AuthenticationError("Unauthenticated")
         }
         const decode = jwt.verify(token, process.env.SECRET)
         if (!decode.userId) {
-            throw new InvariantError("Invalid Token")
+            throw new ClientError("Invalid Token")
         }
         if (decode.role != 'Administrator') {
             throw new AuthorizationError("Anda tidak berhak mengakses resource ini")
         }
-        req.userId = decode.userId
-        req.userName = decode.userName
-        req.role = decode.role
         next()
     } catch (error) {
         next(error)
