8f971efc888eba36d62685bea7f2653d36772c29
iwan0210 <iwancuah13@gmail.com>
2024-06-21 23:55:22 +0700

    update

--- src/middleware/ErrorHandler.js
+++ src/middleware/ErrorHandler.js
@@ -2,7 +2,7 @@ const ClientError = require('../exceptions/ClientError')
 
 const ErrorHandler = (err, _req, res, _next) => {
     const errStatus = err.statusCode || 500;
-    const errMsg = err.message || 'Something went wrong';
+    const errMsg = (err instanceof ClientError) ? err.message : 'Something went wrong';
     res.status(errStatus).json({
         error: true,
         status: errStatus,
